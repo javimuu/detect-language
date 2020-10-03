c9deee0615946d2ee653a37b8e9df55aa869645a
Amit Kumar Mitra <50025230+amit14mitra@users.noreply.github.com>
2020-10-03 22:23:54 +0530

    added a stringFind js file

---
+++ scripts/stringFind.js
@@ -0,0 +1,8 @@
+'use strict'
+
+function stringFind(){
+    var str = "demo string";
+    var txt=document.getElementById('selectId').innerHTML;
+    var n = str.search(text);
+    console.log(n);
+}
