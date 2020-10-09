2543255ea047a14556e19432bef297aad6caad12
Amit Kumar Mitra <amit14mitra@gmail.com>
2020-10-09 21:59:45 +0530

    change done

--- scripts/popup.js
+++ scripts/popup.js
@@ -22,6 +22,8 @@ function findWord() {
 }
 
 function highlightWord() {
+
     chrome.tabs.executeScript( null, {code: 'var str = document.querySelector("body").innerHTML.replace(/[\u3041-\u3096\u30A0-\u30FF\u3400-\u4DB5\u4E00-\u9FCB\uF900-\uFA6A\u2E80-\u2FD5\uFF5F-\uFF9F\u3000-\u303F]/g, `<span class="hightlight__detected">$&</span>`); document.querySelector("body").innerHTML = str;'},
     function(results){ console.log(results); } );
+    
 };
