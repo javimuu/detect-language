d6856f6c8690548e1469822d281b70cfd55ebd79
Amit Kumar Mitra <50025230+amit14mitra@users.noreply.github.com>
2020-10-09 21:53:21 +0530

    file deleted

--- scripts/stringFind.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-function stringFind(){
-    var str = "demo string";
-    var txt=document.getElementById('selectId').innerHTML;
-    var n = str.search(text);
-    console.log(n);
-}
